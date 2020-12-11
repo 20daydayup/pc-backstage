ccede17c040e529bce80cc95aa1c1c44da1bb8fd
20daydayup <13636518634@163.com>
2020-12-11 18:19:03 +0800

    feat:trademark基本样式

--- src/views/product/trademark/list.vue
+++ src/views/product/trademark/list.vue
@@ -1,11 +1,62 @@
 <template>
-<div>
-  品牌管理
-</div>
+  <div>
+    <el-button type="primary" icon="el-icon-plus">添加</el-button>
+    <el-table :data="tableData" border style="width: 100%">
+      <el-table-column type="index" label="序号" width="130" align="center">
+      </el-table-column>
+      <el-table-column prop="name" label="品牌名称"> </el-table-column>
+      <el-table-column label="品牌LOGO">
+        <template slot-scope="scope">
+          <img class="trademark-img" :src="scope.row.logo" alt="logo" />
+        </template>
+      </el-table-column>
+
+      <el-table-column>
+        <template>
+          <el-button type="warning" icon="el-icon-edit">修改</el-button>
+          <el-button type="danger" icon="el-icon-delete">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <el-pagination
+      class="trademark-pagination"
+      :page-sizes="[3, 6, 9]"
+      :page-size="3"
+      layout="prev, pager, next,jumper,sizes,total"
+      :total="1000"
+    >
+    </el-pagination>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'TrademarkList',
-}
+  name: "tradmark",
+  data() {
+    return {
+      tableData: [
+        {
+          name: "王小虎",
+          logo:
+            "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1331594225,2237786756&fm=26&gp=0.jpg",
+        },
+      ],
+    };
+  },
+};
 </script>
+
+<style lang="sass">
+.trademark-img
+  width: 150px
+
+.trademark-pagination
+  text-align: right
+
+.el-pagination__sizes
+  margin-left: 260px
+
+.el-button
+  margin-bottom: 30px
+</style>
